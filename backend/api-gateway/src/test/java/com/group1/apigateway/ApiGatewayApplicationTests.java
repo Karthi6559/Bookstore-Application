deae7daa7fde642e2a2359a49ec5f6813d983021
karthi6559 <karthi6559@gmail.com>
2026-06-14 20:20:57 +0100

    Corrected the third set of security warnings

--- backend/api-gateway/src/test/java/com/group1/apigateway/ApiGatewayApplicationTests.java
+++ backend/api-gateway/src/test/java/com/group1/apigateway/ApiGatewayApplicationTests.java
@@ -2,9 +2,16 @@ package com.group1.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
+
+    // Prevents Spring from calling Keycloak on startup to fetch the JWK Set.
+    // In CI there is no Keycloak, so without this mock the context fails to load.
+    @MockBean
+    ReactiveJwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
